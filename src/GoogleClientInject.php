7316050c1b8aa7154fb6f99e0812233567e651dc
Shingo Kumagai <shingo4092@gmail.com>
2016-07-08 16:29:48 +0900

    Add doc for GoogleClientInject

--- src/GoogleClientInject.php
+++ src/GoogleClientInject.php
@@ -9,10 +9,15 @@ namespace Ray\GoogleModule;
 trait GoogleClientInject
 {
     /**
-     * @var Google_Client
+     * @var \Google_Client
      */
     protected $client;
 
+    /**
+     * @param \Google_Client $client
+     *
+     * @\Ray\Di\Di\Inject
+     */
     public function setGoogleClient(\Google_Client $client = null)
     {
         $this->client = $client;
